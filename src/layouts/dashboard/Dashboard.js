f349af4d7f9d5033b65d4c0f0cd2de73ac8e3ed1
Orie Steele <orie@ohs.io>
2017-04-15 16:05:25 -0500

    Fix demo defect

--- src/layouts/dashboard/Dashboard.js
+++ src/layouts/dashboard/Dashboard.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 class Dashboard extends Component {
   constructor(props, { authData }) {
     super(props)
-    this.props.authData = authData
+    console.log( this.props, authData );
   }
 
   render() {
